9bd48ca08a91ec270d141b1a121601c64384ebdd
Ramdan <ramdan@aleph-labs.com>
2022-05-25 23:27:03 +0700

    [UTS] Init Commit new setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# UTS Pemrograman Mobile
+
+## Data Diri
+* NIM : 30819050
+* Nama : Muhammad Ramdan
+* Tema Aplikasi : Mobile Apps Digital E-Library
---
+++ android/app/src/main/kotlin/com/example/uts_30819050_ramdan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uts_30819050_ramdan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
